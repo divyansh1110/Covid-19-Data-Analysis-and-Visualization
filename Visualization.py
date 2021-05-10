5fb5ab745fe59f10d3eb6152ade352b8927c49bc
divyansh1110 <divyanshchawda1@gmail.com>
2021-05-10 18:43:15 +0530

    Improved Visualization by including different color

--- Visualization.py
+++ Visualization.py
@@ -10,7 +10,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 from dash import no_update
 
-
 # Create a dash application
 app = JupyterDash(__name__)
 JupyterDash.infer_jupyter_proxy_config()
@@ -42,15 +41,7 @@ def df(state):
      
     stat=total_state_wise_daily[['Date','Status',state]]
       
-    coun_cnf = coun[coun.Status == 'Confirmed']
-    coun_rec = coun[coun.Status == 'Recovered']
-    coun_decs = coun[coun.Status == 'Deceased']
-    
-    state_cnf = stat[coun.Status == 'Confirmed']
-    state_rec = stat[coun.Status == 'Recovered']
-    state_decs = stat[coun.Status == 'Deceased']
-
-    return coun_cnf,coun_rec,coun_decs,state_cnf,state_rec,state_decs 
+    return coun,stat 
 
 
 # Application layout
@@ -72,16 +63,12 @@ app.layout = html.Div(children=[
                                          ]),
 
                                 html.Div([
-                                        html.Div([ ], id='coun_cnf'),
-                                        html.Div([ ], id='coun_rec'),
-                                        html.Div([ ], id='coun_decs')
+                                        html.Div([ ], id='coun'),
                                          ]),
 
                                 html.Div([
-                                        html.Div([ ], id='state_cnf'),
-                                        html.Div([ ], id='state_rec'),
-                                        html.Div([ ], id='state_decs')
-                                         ]),
+                                        html.Div([ ], id='state'),
+                                        ]),
 
                                 ])
 
@@ -92,12 +79,8 @@ app.layout = html.Div(children=[
 
 @app.callback(
              [ 
-                Output(component_id='coun_cnf', component_property='children'),
-                Output(component_id='coun_rec', component_property='children'),
-                Output(component_id='coun_decs', component_property='children'),
-                Output(component_id='state_cnf', component_property='children'),
-                Output(component_id='state_rec', component_property='children'),
-                Output(component_id='state_decs', component_property='children')
+                Output(component_id='coun', component_property='children'),
+                Output(component_id='state', component_property='children'),
               ], 
               [Input(component_id='state-type', component_property='value'),
               ]
@@ -108,32 +91,18 @@ app.layout = html.Div(children=[
 
 def get_graph(state):
 
-    coun_cnf,coun_rec,coun_decs,state_cnf,state_rec,state_decs=df(state)    
+    co,st=df(state)    
 
-    coun_cnf_fig = px.line(coun_cnf, x='Date', y='Total', title='Total Confirmed cases in India')
-    coun_rec_fig = px.line(coun_rec, x='Date', y='Total',  title='Total recovery in India')
-    coun_decs_fig = px.line(coun_decs, x='Date', y='Total', title='Total Deaths in India')
-    state_cnf_fig = px.line(state_cnf, x='Date', y=state, title=' Confirmed cases in State')
-    state_rec_fig = px.line(state_rec, x='Date', y=state, title='Total recovery in State')
-    state_decs_fig = px.line(state_decs, x='Date', y=state, title='Total recovery in State')
+    coun_fig = px.line(co, x='Date',color='Status', y='Total', title='Total Confirmed,recovery,Deaths cases in India')
+    state_fig = px.line(st, x='Date', y=state,color='Status', title='Total Confirmed,recovery,Deaths in State')
  
                    
     return [
-            dcc.Graph(figure=coun_cnf_fig), 
-            dcc.Graph(figure=coun_rec_fig),
-            dcc.Graph(figure=coun_decs_fig),
-            dcc.Graph(figure=state_cnf_fig),
-            dcc.Graph(figure=state_rec_fig),
-            dcc.Graph(figure=state_decs_fig)        
+            dcc.Graph(figure=coun_fig), 
+            dcc.Graph(figure=state_fig),        
            ]
-
-
-
-
-
 
 
 # Run the app
 if __name__ == '__main__': 
-    # REVIEW8: Adding dev_tools_ui=False, dev_tools_props_check=False can prevent error appearing before calling callback function
     app.run_server(mode="inline", host="localhost", debug=False, dev_tools_ui=False, dev_tools_props_check=False)
